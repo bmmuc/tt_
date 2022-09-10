4286077bc6bfbd78242152886c44863fe9b6d1f8
bmmuc <bmmuc@cin.ufpe.br>
2022-09-09 21:05:04 -0300

    v0 working

--- 1.c
+++ 1.c
@@ -2,23 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *files_available; // array with the id of the files available
-int *global_votes;
+int *files_available = NULL; // array with the id of the files available
+int *global_votes = NULL;
 
-int n; // number of files
-int t; // number of threads
-int c; // number of candidates
+int n = 0; // number of files
+int t = 0; // number of threads
+int c = 0; // number of candidates
+
+int votes_sum = 0;
 int number_of_files_read = 0;
-pthread_mutex_t *mymutexs; // array of mutexs
+pthread_mutex_t *mymutexs = NULL; // array of mutexs
 pthread_mutex_t mutex;
+
 typedef struct file_info {
   int len;
   int *votes;
 } file_info;
 
-// here we will return the top of the array and remove it from the array
 int decite_file_available() {
-  printf("verificando arquivo livre\n");
+  // printf("verificando arquivo livre\n");
   pthread_mutex_lock(&mutex);
 
   int i = 1;
@@ -33,14 +35,14 @@ int decite_file_available() {
   }
 
   pthread_mutex_unlock(&mutex);
-  printf("arquivo livre %d\n", file);
+  // printf("arquivo livre %d\n", file);
   return file;
 }
-// the read file recieves an id and the name of each file is data/id.in
+
 file_info readFile(int id) {
-  printf("lendo arquivo data/%d.in\n", id);
+  // printf("lendo arquivo data/%d.in\n", id);
   char file_name[100];
-  sprintf(file_name, "data/%d.in", id);
+  sprintf(file_name, "data2/%d.in", id);
 
   FILE *file = fopen(file_name, "r");
 
@@ -64,46 +66,63 @@ file_info readFile(int id) {
 };
 
 void *count_votes(void *threadid) {
-  // printf("Thread %d started", (int)&threadid);
   int file = decite_file_available();
   while (file != -1) {
     file_info info = readFile(file);
     int i = 0;
-    // printf("contando votos do arquivo %d\n", info.len);
     for (; i < info.len; i++) {
-      // printf("info vote: %d do arquivo: %d, da thread: %d\n", info.votes[i],
-      //        file, (int)threadid);
       pthread_mutex_lock(&mymutexs[info.votes[i]]);
       global_votes[info.votes[i]] = global_votes[info.votes[i]] + 1;
+      votes_sum++;
       pthread_mutex_unlock(&mymutexs[info.votes[i]]);
     }
     file = decite_file_available();
   }
 };
 
+int findMaxIndex() {
+  int i = 1;
+  int max = 0;
+  int max_index = 0;
+  for (; i <= c; i++) {
+    if (global_votes[i] > max) {
+      max = global_votes[i];
+      max_index = i;
+    }
+  }
+  return max_index;
+}
+
 int main() {
   printf("insira o numero de arquivos: ");
   scanf("%d", &n);
 
   printf("insira o numero de threads: ");
-  scanf("%d", &t);
+  scanf(" %d", &t);
 
   printf("insira o numero de candidatos: ");
-  scanf("%d", &c);
+  scanf(" %d", &c);
+
+  printf("numero de arquivos: %d\n", n);
+  printf("numero de threads: %d\n", t);
+  printf("candidatos: %d\n", c);
 
   files_available = (int *)malloc(n * sizeof(int));
+
   int i = 1;
   for (; i <= n; i++) {
+    printf("arquivo %d disponivel\n", i);
     files_available[i] = i;
   }
-
+  printf("arquivos disponiveis\n");
   mymutexs = (pthread_mutex_t *)malloc((c + 1) * sizeof(pthread_mutex_t));
+  printf("mutexs criados\n");
   for (i = 0; i < c + 1; i++) {
     pthread_mutex_init(&mymutexs[i], NULL);
   }
   printf("criei o array mutex\n");
 
-  global_votes = (int *)malloc(c * sizeof(int));
+  global_votes = (int *)malloc((c + 1) * sizeof(int));
   for (i = 0; i < c + 1; i++) {
     global_votes[i] = 0;
   }
@@ -117,7 +136,14 @@ int main() {
     pthread_join(threads[i], NULL);
   }
 
+  printf("votos totais: %d\n", votes_sum);
+
   for (i = 0; i < c + 1; i++) {
-    printf("candidate: %d with %d votes\n", i, global_votes[i]);
+    printf("candidato %d com %.2f%%(%d)dos votos\n", i,
+           (float)global_votes[i] / votes_sum * 100, global_votes[i]);
   }
+  int index = findMaxIndex();
+  // {3: 6, 4: 10, 2: 5, 0: 3, 1: 2, 5: 3}
+
+  printf("candidato vencedor: %d\n", index);
 }
---
+++ populate.py
@@ -0,0 +1,25 @@
+# creates a function that recieve four arguments
+# the first argument is the number of files to be created.
+# the second argument is the range of the numbers to be write in the files.
+# the third argument is the path where the files will be created.
+# the fourth argument is the number of lines to be write in the files.
+# each line has a number between 0 and the range.
+# a file can has a number of lines between 1 and the number of lines.
+import os
+import random
+
+def populate(n, r, path, l):
+    nums = {}
+    for i in range(1,n + 1):
+        with open(os.path.join(path, str(i) + ".in"), "w") as f:
+            lines = random.randint(1, l)
+            f.write(str(lines) + "\n")
+            for j in range(lines):
+                num = random.randint(0, r)
+                f.write(str(num) + "\n")
+                if num in nums:
+                    nums[num] += 1
+                else:
+                    nums[num] = 1
+    print(nums)
+populate(5, 5, 'data2/', 15)
